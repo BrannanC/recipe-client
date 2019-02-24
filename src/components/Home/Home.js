5691dc14d58d0ba9cbea98c4a2f5e2368fbe4da1
BrannanC <brannanconrad@gmail.com>
2019-02-23 23:15:32 -0600

    Fixed file structure

---
+++ src/components/Home/Home.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import DishListView from '../Dishes/DishListView';
+
+import Axios from 'axios';
+
+class Home extends Component {
+  state = {
+    dishes: [],
+    error: ''
+  }
+
+  componentDidMount(){
+    Axios
+      .get('https://brannan-recipe.herokuapp.com/api/dishes')
+      .then(res => {
+        this.setState({
+          dishes: res.data.dishes
+        })
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({
+          error: err
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div className="Home">
+      <h1>Recipe Books Dishes</h1>
+        <div className="dish-list">{this.state.dishes.map(x => <DishListView dish={x} key={x.id} />)}</div>
+      </div>
+    );
+  }
+}
+
+export default Home;
